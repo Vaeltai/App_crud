f8beb5df09aed70c93ce63de638828faa1b58282
Vaeltai <70526462+Vaeltai@users.noreply.github.com>
2021-02-14 19:44:04 +0300

    Delete src directory

--- src/main/java/config/Application.java
+++
@@ -1,8 +0,0 @@
-package config;
-
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-
-public class Application {
-    ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
-}
