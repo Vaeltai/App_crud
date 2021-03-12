dc51bc6da1ee5edbfafcbe0505d89a9b50dbd957
Vaeltai <70526462+Vaeltai@users.noreply.github.com>
2021-03-12 14:30:50 +0300

    Delete Application.java

--- src/main/java/web/config/Application.java
+++
@@ -1,8 +0,0 @@
-package web.config;
-
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-
-public class Application {
-    ApplicationContext applicationContext = new AnnotationConfigApplicationContext(WebConfig.class);
-}
